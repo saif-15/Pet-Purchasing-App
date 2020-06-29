46d815566d08133a50a727daa62f9f618883d226
shady <saifulhaq.15@outlook.com>
2020-06-29 15:54:51 +0500

    initial commit

--- lib/colors.dart
+++ lib/colors.dart
@@ -4,6 +4,6 @@ Color mainBgColor=Color(0xFF406C6C);
 Color lightBgColor=Color(0xFFC7D1D4);
 Color lightMainBgColor=Color(0x0F406C6C);
 
-Color lightBrown=Color(0x91B5651D);
-Color lightGray=Color(0x91333333);
-Color darkBrown=Color(0x91B5651D);
+Color lightBrown=Color(0x9FB5651D);
+Color lightGray=Color(0x9F333333);
+Color darkBrown=Color(0x9FB5651D);
